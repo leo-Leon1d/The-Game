66cad8068a2b09f3c0e124595cc928d3d745e3e0
Israelyan Leonid <leo.israelyan@icloud.com>
2021-04-23 16:13:29 +0300

    Initial Commit

---
+++ TheGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheGame
+//
+//  Created by Леонид Исраелян on 23.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
